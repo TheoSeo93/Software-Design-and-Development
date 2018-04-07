c63b960717656caeae6fe996e231594d50763abc
Theo <youngho.seo@stonybrook.edu>
2018-04-07 18:11:05 -0400

    Imported the files uploaded from professor Ritwik Banerjee
    -Algorithm.java
    -Classifier.java
    -RandomClassifier.java
    -DataSet.java

---
+++ hw1/data-vilij/src/classification/Algorithm.java
@@ -0,0 +1,16 @@
+package classification;
+/**
+ * This interface provides a way to run an classification
+ * on a thread as a {@link java.lang.Runnable} object.
+ *
+ * @author Ritwik Banerjee
+ */
+public interface Algorithm extends Runnable {
+
+    int getMaxIterations();
+
+    int getUpdateInterval();
+
+    boolean tocontinue();
+
+}
